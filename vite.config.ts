48d0e33896b0a0f398a95d4bb7a4a988bfb1aaa8
SharedBBT <abs@bbt757.com>
2026-01-20 15:40:09 +0900

    run build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // <--- 必须要有这一行，且必须是 './'
 })
